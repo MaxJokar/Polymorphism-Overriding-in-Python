2db80d18f32c92eef9842c95deafb903a5ceee35
email <mmb2020max@gmail.com>
2022-02-22 14:43:16 +0200

    Done

--- Polymorphism Overriding.py
+++ Polymorphism Overriding.py
@@ -3,15 +3,48 @@ class A:
     def show(self):
         print("AAAAAA")
         
-class B(A):
+class B:
     def show(self):
         print("BBBBB")        
+        # return A.show(self)
+     
+     
+     
+class C(B,A):
+    def show(self):
+       print("CCCC")
+       return A.show(self)  
+           
+     
+     
+c=C()
+c.show()
+super(C,c).show()   
+     
+     
+     
+     
+     
+     
+     
         
-        
-a=A()
-a.show()  
+# a=A()
+# a.show()  
 
-b=B()
-b.show()         
 
-super(B,b).show()
+
+# b=B()
+# b.show()         
+# #go to the Parent of  B and  its self is b, execute function show
+# super(B,b).show()
+
+
+
+
+
+
+
+
+
+
+
